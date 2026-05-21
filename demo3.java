de486e631cacb9052fbb46a341a87f3758345885
Deepjyot Singh <deepjyotsingh49@gmail.com>
2026-05-21 21:11:02 +0530

    updated java files

---
+++ demo3.java
@@ -0,0 +1,31 @@
+public class demo3 {
+    public static void main(String[] args) {
+        int num [][]=new int[3][4];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                num[i][j]=(int)(Math.random()*10);
+                System.out.println(num[i][j]+" ");
+            }
+                
+        }
+         for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                
+                System.out.print(num[i][j]+" ");
+            }
+            System.out.println();
+
+            
+                
+        }
+        System.out.println("by using enhance for loop ------------------------------");
+        
+        for(int n[]:num){
+            for(int m:n){
+                System.out.print(m+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
