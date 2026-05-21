6e514b1851d8140ec8fc4d715f4004a01b86a57e
Deepjyot Singh <deepjyotsingh49@gmail.com>
2026-05-21 20:35:18 +0530

    essagejava files uploaded

---
+++ demo.java
@@ -0,0 +1,12 @@
+public class demo {
+    public static void main(String args[]){
+    //int arr[]={1,2,3};
+    int arr[]=new int[4];
+    arr[0]=10;
+    arr[1]=20;
+    arr[2]=30;
+    arr[3]=40;
+    System.out.println(arr[0]);
+    }
+    
+}
