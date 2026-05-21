c2de5762156e278c7a0ef678cc98b398489da392
Deepjyot Singh <deepjyotsingh49@gmail.com>
2026-05-21 14:39:03 +0530

    updated calculator program

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,18 @@
+class demo{
+    public int add (int a,int b){
+        int r=a+b;
+        return r;
+
+    }
+}
 public class Calculator {
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+        demo dem=new demo();
+        int result=dem.add(num1,num2);
+
+        System.out.println("The sum is: "+result);
+    }
     
 }
