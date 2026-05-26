19be8311efc044b306e5e84b6b32f2aa61ceada2
Deepjyot Singh <deepjyotsingh49@gmail.com>
2026-05-26 20:35:14 +0530

    multilevel inheritance

--- demo13.java
+++ demo13.java
@@ -1,4 +1,6 @@
 
+ // multilevel inheritance
+
  class demo14 {
      public int add(int n1,int n2){
         return n1+n2;
@@ -17,17 +19,24 @@ class demo15 extends demo14 {
     }
     
 }
+class demo16 extends demo15{
+    public double power (int n1,int n2){
+        return Math.pow(n1, n2);
+    }
+}
 public class demo13 {
     public static void main(String args[]){
-        demo14 d1=new demo14();
+        demo16 d1=new demo16();
         int sum=d1.add(10, 20);
         int diff=d1.sub(20, 10);
         int multi=d1.multi(10, 20);
         int div=d1.div(20, 10);
+        double power=d1.power(10, 20);
         System.out.println("sum: "+sum);
         System.out.println("difference: "+diff);
         System.out.println("product: "+multi);
         System.out.println("quotient: "+div);
+        System.out.println("power: "+power);
     }
     
 }
