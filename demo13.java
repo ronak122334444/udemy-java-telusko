5d7e3933a1917b7e4e10af33b712c8d62dcc4f26
Deepjyot Singh <deepjyotsingh49@gmail.com>
2026-05-26 20:22:52 +0530

    java files updated

---
+++ demo13.java
@@ -0,0 +1,33 @@
+
+ class demo14 {
+     public int add(int n1,int n2){
+        return n1+n2;
+    }
+    public int sub(int n1,int n2){
+        return n1-n2;
+    }
+    
+}
+class demo15 extends demo14 {
+    public int multi(int n1,int n2){
+        return n1*n2;
+    }
+    public int div(int n1,int n2){
+        return n1/n2;
+    }
+    
+}
+public class demo13 {
+    public static void main(String args[]){
+        demo14 d1=new demo14();
+        int sum=d1.add(10, 20);
+        int diff=d1.sub(20, 10);
+        int multi=d1.multi(10, 20);
+        int div=d1.div(20, 10);
+        System.out.println("sum: "+sum);
+        System.out.println("difference: "+diff);
+        System.out.println("product: "+multi);
+        System.out.println("quotient: "+div);
+    }
+    
+}
