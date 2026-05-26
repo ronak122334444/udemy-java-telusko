7f370f1de0b7715addcb0419bd90bd1da73a6fb5
Deepjyot Singh <deepjyotsingh49@gmail.com>
2026-05-26 16:07:19 +0530

    java files uploaded

---
+++ demo10.java
@@ -0,0 +1,28 @@
+//static means you are making something as a class member 
+class Mobile{
+
+    String brand;
+    int price;
+   static String name;
+    public void show(){
+        System.out.println("brand: "+brand);
+        System.out.println("price: "+price);
+        System.out.println("name: "+name);
+    }
+}
+public class demo10 {
+    public static void main (String args[]){
+        Mobile m1=new Mobile();
+        m1.brand="samsung";
+        m1.price=20000;
+        Mobile.name="Galaxy S21";
+        m1.show();
+        Mobile m2=new Mobile();
+        m2.brand="apple";   
+        m2.price=100000;
+        m2.name="iPhone 13";
+        m2.show();
+    }
+
+    
+}
