1f10ac81a281a814889733a8ea25dcc1dab127e6
Deepjyot Singh <deepjyotsingh49@gmail.com>
2026-05-29 04:15:57 +0530

    exception handling

---
+++ demo28.java
@@ -0,0 +1,16 @@
+public class demo28 {
+    public static void main(String args[]){
+        double i=4;
+        double j=0;
+        try{
+            j=18/i;
+        }
+        catch(Exception ex){
+            System.out.println("went wrong");
+                
+        }
+      System.out.println(j);
+      System.out.println("bye");
+    }
+    
+}
